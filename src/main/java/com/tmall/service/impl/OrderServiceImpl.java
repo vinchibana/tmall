45f7deffb73d3dfd0a3f47107f6a580c2dfb6c6d
shuwuai <vialin1128@gmail.com>
2018-02-18 03:19:29 +0800

    Pre-publishing test3

--- src/main/java/com/tmall/service/impl/OrderServiceImpl.java
+++ src/main/java/com/tmall/service/impl/OrderServiceImpl.java
@@ -28,11 +28,13 @@ import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
+@Service("iOrderService")
 public class OrderServiceImpl implements IOrderService {
     
     private static final Logger logger = LoggerFactory.getLogger(OrderServiceImpl.class);
